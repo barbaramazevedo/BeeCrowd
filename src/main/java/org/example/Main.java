4e487e8db5324ae0c21fc54212bf9c373ef18cb1
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2026-01-13 12:00:02 -0300

    Remove comments messages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,9 +10,6 @@ public class Main {
         double Y = sc.nextDouble();
         double average = (double) X/Y;
 
-//        int maiorAB = (a + b + Math.abs(a - b)) / 2;
-//        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) /2;
-
         System.out.printf(Locale.US, "%.3f km/l%n", average);
 
         sc.close();
